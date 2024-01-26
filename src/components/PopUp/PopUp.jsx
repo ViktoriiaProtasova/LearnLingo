00946de1e75fbad7b36f430f96bca49d8e78697c
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-01-26 18:40:53 +0200

    Update PopUp.jsx

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyledPopUp } from './PopUp.styled';
 import { CgClose } from 'react-icons/cg';
-import { iconSize } from 'components/constants';
+import { iconSize } from 'components/Constants';
 
 const PopUp = ({ title, text, children }) => {
   return (
