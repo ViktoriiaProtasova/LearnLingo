82291d94147c7ca8056ddd95d236769a4211f75e
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-01-30 15:25:37 +0200

    constants was updated

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyledPopUp } from './PopUp.styled';
 import { CgClose } from 'react-icons/cg';
-import { iconSize } from 'components/Constants';
+import { iconSize } from 'components/Constant';
 
 const PopUp = ({ title, text, children }) => {
   return (
