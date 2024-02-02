d8d89269292af4d3b7a684a3944d6d51d62778d0
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-02-02 12:30:45 +0200

    components were updated

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/400-italic.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 // import Button from '../Button/Button';
 import PopUp from '../PopUp/PopUp';
 import Booking from 'components/Booking/Booking';
 import LoginCard from 'components/LoginCard/LoginCard';
+import RegistrationCard from 'components/RegistrationCard/RegistrationCard';
+import TeacherCard from 'components/TeacherCard/TeacherCard';
 
 const App = () => {
   return (
     <div>
       {/* <Button $registration>Click me</Button> */}
-      <PopUp>
-        {/* <LoginCard></LoginCard> */}
+      {/* <PopUp>
+        <LoginCard></LoginCard>
         <Booking></Booking>
-      </PopUp>
+        <RegistrationCard></RegistrationCard>
+      </PopUp> */}
+      <TeacherCard></TeacherCard>
     </div>
   );
 };
--- src/components/RegistrationCard/RegistrationCard.jsx
+++ src/components/RegistrationCard/RegistrationCard.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { StyledTitle } from 'components/Booking/Booking.styled';
+import { StyledInput } from 'components/Input/Input.styled';
+import { StyledRegistrationCard } from './RegistrationCard.styled';
+import Button from 'components/Button/Button';
 
 const RegistrationCard = () => {
-  return <div></div>;
+  return (
+    <StyledRegistrationCard>
+      <StyledTitle>
+        <h1 className="form-title">Registration</h1>
+        <p className="form-text">
+          Thank you for your interest in our platform! In order to register, we
+          need some information. Please provide us with the following
+          information
+        </p>
+      </StyledTitle>
+      <fieldset className="input-group">
+        <StyledInput name="name" type="text" placeholder="Name"></StyledInput>
+        <StyledInput
+          name="email"
+          type="email"
+          placeholder="Email"
+        ></StyledInput>
+        <StyledInput
+          name="password"
+          type="password"
+          placeholder="Password"
+        ></StyledInput>
+      </fieldset>
+      <Button $size="large">Sign Up</Button>
+    </StyledRegistrationCard>
+  );
 };
 
 export default RegistrationCard;
