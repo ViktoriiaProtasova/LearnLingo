db01651d1b36c131a8f85e77b85968af3cd7f5b6
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-02-05 15:42:21 +0200

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { StyledHero } from './Hero.styled';
+import Button from 'components/Button/Button';
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <StyledHero>
+      <h1 className="hero-title">
+        Unlock your potential with the best <span>language</span> tutors
+      </h1>
+      <p className="hero-text">
+        Embark on an Exciting Language Journey with Expert Language Tutors:
+        Elevate your language proficiency to new heights by connecting with
+        highly qualified and experienced tutors.
+      </p>
+      <Button>Get started</Button>
+    </StyledHero>
+  );
 };
 
 export default Hero;
