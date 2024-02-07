54da121f7cf1d9116082799c10d4c177d3040cde
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-02-07 21:26:01 +0200

    Create color.js

---
+++ src/constants/color.js
@@ -0,0 +1,14 @@
+export const color = Object.freeze({
+  secondaryYellow: '#ffdc86',
+  primaryYellow: '#f4c550',
+  lightYellow: '#fbe9ba',
+  secondaryTextColor: ',ffffff',
+  primaryTextColor: '#121417',
+  lightTextColor: '#8a8a89',
+  primaryDark: 'rgba(18, 20, 23, 0.8)',
+  accentColor: '#38cd3e',
+  primaryMidnight: ' rgba(18, 20, 23, 0.7)',
+  primaryMiddle: ' rgba(18, 20, 23, 0.2)',
+  primaryLight: ' rgba(18, 20, 23, 0.1)',
+  bgColor: '#f8f8f8',
+});
