177fe48a31f0d47cd81228858ae33c6b67661df6
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-02-22 14:38:45 +0200

    Responsive images were added

--- src/components/HeroImg/HeroImg.jsx
+++ src/components/HeroImg/HeroImg.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import hero from '../../images/block-1.jpg';
+import hero2x from '../../images/block-1@2x.jpg';
+import hero3x from '../../images/block-1@3x.jpg';
+
 import { StyledHeroImg } from './HeroImg.styled';
 
 const HeroImg = () => {
   return (
     <StyledHeroImg>
-      <img src={hero} alt="Girl with laptop" />
+      <img
+        srcSet={`${hero} 568w, ${hero2x} 1136w, ${hero3x} 1704w`}
+        src={hero}
+        sizes="100vw"
+        alt="Girl with a laptop"
+      />
     </StyledHeroImg>
   );
 };
