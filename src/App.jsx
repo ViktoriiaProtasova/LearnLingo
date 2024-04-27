0d5d63ba2e16a1be76a4db920fbf75d46399221f
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-04-27 22:39:46 +0300

    Directory was changed

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
       {/* <RegistrationCard /> */}
       {/* </PopUp> */}
       {/* <TeacherCard /> */}
-      {/* <Hero /> */}
+      <Hero />
       {/* <HeroImg /> */}
       {/* <Login></Login> */}
       {/* <Logo></Logo> */}
